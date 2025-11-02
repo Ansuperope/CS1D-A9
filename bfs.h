63bb5ad5b4785ef9350c221adfa974a1e9ab2899
Ansuperope <151668459+Ansuperope@users.noreply.github.com>
2025-11-02 17:51:29 +0000

    set up files didn't do anything yet

---
+++ bfs.h
@@ -0,0 +1,13 @@
+/***************************************************************************
+ * bfs.h
+ *------------------------------------------------------------------------
+ * Header file for Breadth-First Search (BFS) algorithm
+ **************************************************************************/
+#ifndef BFS_H
+#define BFS_H
+#include <vector>
+#include <queue>
+#include <unordered_set>
+#include <iostream>
+
+#endif // BFS_H
---
+++ main.cpp
@@ -0,0 +1,16 @@
+/***************************************************************************
+ * main.cpp
+ *--------------------------------------------------------------------------
+ * By Aspen Cristobal and Amy
+ *--------------------------------------------------------------------------
+ * Main source file for Breadth-First Search (BFS) algorithm
+ **************************************************************************/
+#include "bfs.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Example usage of BFS
+    return 0;
+} // END main
